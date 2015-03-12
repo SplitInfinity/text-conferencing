b22fc9e36903dd3dc5d2e3fe8f44be8f114d213e
Arash Khazaei <arash.khazaei@gmail.com>
2015-03-11 21:53:17 -0400

    created a file that stores all potential errors w/ their corresponding errno

---
+++ server/err.h
@@ -0,0 +1,19 @@
+#ifndef ERR_H
+#define ERR_H
+
+
+
+#define SESSION_NOT_EXIST 			501
+#define BAD_SESSION					504
+#define SESSION_EXISTS				511
+#define CLIENT_EXISTS				611
+#define CLIENT_NOT_EXIST			601
+#define BAD_CLIENT 					604
+#define SERVER_PARAM_ERROR			747
+#define SERVER_BROADCAST_ERROR  	700
+#define SERVER_NO_SESSIONS			1000
+#define SERVER_SUCCESS 				0
+#define SERVER_ERROR 				-1
+
+
+#endif
